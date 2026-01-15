a64342336d299cbeb932b255703e9b99a60876df
Finn Lucas Griggs <finn.griggs3@gmail.com>
2026-01-15 13:25:53 +0100

    Removed stock reference

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -4,7 +4,7 @@ import moment from "moment";
 import HomeAvatar from "../standalone/home-avatar/home-avatar";
 import Stocks from "../widgets/stocks/stocks";
 
-const Header : React.FC = () => {
+const Header: React.FC = () => {
   const [minutes, setMinutes] = useState(moment().format("HH:mm:ss"));
   useEffect(() => {
     const countdownInterval = setInterval(() => {
@@ -16,29 +16,33 @@ const Header : React.FC = () => {
 
   return (
     <div className="header-container">
-          <div className="header-content-container" >
-              <div className="header-row">
-                <h1 className="header-row-item left"><strong>Stensberggata 21</strong></h1>
-                <div className="header-row-item right">
-                  <h1>{minutes}</h1></div>
-              </div>
-              <div className="header-row">
-                <div className="header-row-item avatars">
-                    <div className="header-avatar"> <HomeAvatar name="finn_griggs" /></div>
-                    <div className="header-avatar"><HomeAvatar name="pernille" /></div>
-                    <div className="header-avatar"><HomeAvatar name="line" /></div>
-                </div>
-                <div className="header-row-item right stock-container">
-                  <Stocks />
-                </div>
-              </div>
-              
-            
-              
+      <div className="header-content-container">
+        <div className="header-row">
+          <h1 className="header-row-item left">
+            <strong>Stensberggata 21</strong>
+          </h1>
+          <div className="header-row-item right">
+            <h1>{minutes}</h1>
           </div>
-          
+        </div>
+        <div className="header-row">
+          <div className="header-row-item avatars">
+            <div className="header-avatar">
+              {" "}
+              <HomeAvatar name="finn_griggs" />
+            </div>
+            <div className="header-avatar">
+              <HomeAvatar name="pernille" />
+            </div>
+            <div className="header-avatar">
+              <HomeAvatar name="line" />
+            </div>
+          </div>
+          <div className="header-row-item right stock-container">{/* <Stocks /> */}</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Header;
