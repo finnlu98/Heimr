429abffd1c2a59683dd94ce466619dd5f5cdd138
Finn Lucas Griggs <finn.griggs3@gmail.com>
2026-01-16 20:28:15 +0100

    added build and deploy strat

--- client/src/api/ApiClient.ts
+++ client/src/api/ApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_HEIMR_BACKEND_ENDPOINT ?? "",
+  baseURL: process.env.REACT_APP_HEIMR_BACKEND_ENDPOINT ?? "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
